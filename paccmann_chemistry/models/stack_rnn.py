fix: bug in stack that computes softmax over baatch

paccmann_chemistry/models/stack_rnn.py
@@ -116,8 +116,8 @@ class StackGRU(nn.Module):
             Stack of size `[batch_size, stack_depth, stack_width]`.
         """
         if input_token.shape[0] != 1 or len(input_token.shape) < 2:
-            # It may receive a single token as input so the fist element is 1
-            # but actially corresponding to batch size. In that case we also
+            # It may receive a single token as input so the first element is 1
+            # but actually corresponding to batch size. In that case we also
             # resize.
             input_token = input_token.view(1, -1)
         embedded_input = self.encoder(input_token.to(self.device))
@@ -134,10 +134,8 @@ class StackGRU(nn.Module):
 
     def _stack_update(self, embedded_input, hidden, stack):
         """Pre-gru stack update operations"""
-        stack_controls = self.stack_controls_layer(
-            hidden[-1, :, :].unsqueeze(0)
-        )
-        stack_controls = F.softmax(stack_controls, dim=1)
+        stack_controls = self.stack_controls_layer(hidden[-1, :, :])
+        stack_controls = F.softmax(stack_controls, dim=-1)
         stack_input = self.stack_input_layer(hidden[-1, :, :].unsqueeze(0))
         stack_input = torch.tanh(stack_input)
         stack = self.stack_augmentation(

paccmann_chemistry/models/tests/test_vae.py
@@ -13,13 +13,13 @@ class TestStackGRUEncoder(unittest.TestCase):
     default_params = {
         'latent_dim': 128,
         'input_size': 12,
-        'rnn_cell_size': 64,
+        'rnn_cell_size': 30,
         'embedding_size': 80,
         'output_size': 100,
         'stack_width': 50,
         'stack_depth': 51,
         'n_layers': 3,
-        'dropout': 1,
+        'dropout': .7,
         'batch_size': 32,
         'bidirectional': True
     }
@@ -55,12 +55,13 @@ class TestStackGRUEncoder(unittest.TestCase):
 
     def test__no_batch_mismatch__in__encoder_train_step(self) -> None:
         """Tests if there is any difference between equal samples in the same
-        batch. If so it may indicate some crosstalk between samples in the 
+        batch. If so it may indicate some crosstalk between samples in the
         batch
         """
         params = self.default_params
         batch_size = params['batch_size']
         gru_stack = StackGRUEncoder(params)
+        gru_stack.eval()
 
         base_sample = np.arange(10)
         base = [torch.Tensor(base_sample) for _ in range(batch_size)]

paccmann_chemistry/models/vae.py
@@ -80,10 +80,12 @@ class StackGRUEncoder(StackGRU):
         return mu, logvar
 
     def _post_gru_reshape(self, hidden: torch.Tensor) -> torch.Tensor:
-        expected_shape = torch.tensor([
-            self.n_layers * self.n_directions, self.batch_size,
-            self.rnn_cell_size
-        ])
+        expected_shape = torch.tensor(
+            [
+                self.n_layers * self.n_directions, self.batch_size,
+                self.rnn_cell_size
+            ]
+        )
         if not torch.equal(torch.tensor(hidden.shape), expected_shape):
             raise ValueError(
                 f'GRU hidden layer has incorrect shape: {hidden.shape}. '