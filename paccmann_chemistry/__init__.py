99244217666f523394012ffb287e40eba7b3a363
Jannis Born <jab@zurich.ibm.com>
2020-09-11 20:04:07 +0200

    feat: Automatic package version fetching

--- paccmann_chemistry/__init__.py
+++ paccmann_chemistry/__init__.py
@@ -1,2 +1,2 @@
 name = 'paccmann_chemistry'
-__version__ = '0.0.2'
+__version__ = '0.0.4'
--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 """Install package."""
-from setuptools import setup, find_packages
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError('Unable to find version string.')
+
 
 setup(
     name='paccmann_chemistry',
-    version='0.0.2',
+    version=get_version('paccmann_chemistry/__init__.py'),
     description='Generative models of chemical data for PaccMann^RL',
     long_description=open('README.md').read(),
     url='https://github.com/PaccMann/paccmann_chemistry',
